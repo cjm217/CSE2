5164efe68709b52eb3692201e17ffef3ba611cab
cjm217 <cjm217@lehigh.edu>
2014-08-29 14:27:29 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Chris Mueller
+//Welcome Class Java Program
+//
+// first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass//
+
+    // define a class
+    public class WelcomeClass{
+
+    // add main method
+    public static void main(String[] args) {
+
+    // print the statement with welcome above it
+    System.out.println("-----------");
+    System.out.println("| Welcome |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-c--j--m--2--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println(" This summer I worked as an intern at G&W Electric Company. I hope to work as an Industrial Engineer in the future.");
+    System.out.println("test");
+    
+    //test
+    }
+        
+}    
