bdb76ad7bbdb558bf87fc7dcde5768a13b407c3e
cjm217 <cjm217@lehigh.edu>
2014-10-01 15:28:16 +0000

    hw05

---
+++ hw05/BoolaBoola.java
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////
+//Chris Mueller
+//CSE 2
+//Boola Boola program
+//
+
+//Import the scanner
+import java.util.Scanner;
+
+//Define the class
+public class BoolaBoola {
+    
+    //Create a main method
+    public static void main(String[] args) {
+        
+        
+    }
+}
